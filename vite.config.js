02764bb053d2cbbec32ea485247737bd04dbe349
mohd-adnaan <iamadnaanmohd@gmail.com>
2026-01-04 03:02:04 -0500

    fix-multi-page routing

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/EEGSS/', 
+  base: '/EEGSS/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        events: resolve(__dirname, 'events.html')
+      }
+    }
+  }
 })
